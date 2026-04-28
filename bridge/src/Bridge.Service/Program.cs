8c21e976dee13c9479d18f6def914301b75822b1
Mizin Jr <samir@bridiproducoes.com.br>
2026-04-27 21:32:37 -0300

    Update Program.cs

--- bridge/src/Bridge.Service/Program.cs
+++ bridge/src/Bridge.Service/Program.cs
@@ -1,6 +1,10 @@
 using Bridge.Service;
 
 var builder = Host.CreateApplicationBuilder(args);
+if (OperatingSystem.IsWindows())
+{
+    builder.Services.AddWindowsService(options => { options.ServiceName = "USB MIDI Bridge"; });
+}
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
